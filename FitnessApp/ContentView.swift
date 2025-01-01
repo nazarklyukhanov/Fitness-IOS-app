037b2b2027808f14f0b24f91588f39819c4b6706
nazarklyukhanov <nklyukhanov@gmail.com>
2025-01-01 18:29:59 +0300

    first one! no sence

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("embrace the right way")
         }
         .padding()
     }
